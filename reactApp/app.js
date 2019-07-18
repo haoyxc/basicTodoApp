e6665a84d6bdfcf4ef7563eaf8231dc7a4858030
haoyxc <48307028+haoyxc@users.noreply.github.com>
2019-07-18 14:24:50 -0400

    TodoApp component added

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,17 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-let dummyData = ["cry", "eat", "sleep"];
+let dummyData = [
+  { taskText: "cry", completed: false },
+  { taskText: "eat", completed: false },
+  { taskText: "sleep", completed: false }
+];
 class Todo extends React.Component {
+  toggleComplete(e) {
+    console.log("hello");
+  }
   render() {
-    return <li>{this.props.task}</li>;
+    return (
+      <li>
+        <input
+          type="checkbox"
+          name="toggleComplete"
+          id=""
+          onClick={event => this.toggleComplete(event)}
+        />
+        {!this.props.task.completed ? (
+          this.props.task.taskText
+        ) : (
+          <strike>{this.props.task.taskText}</strike>
+        )}
+      </li>
+    );
   }
 }
 
 class TodoList extends React.Component {
   render() {
     return (
-      <ul>
+      <ul className="todolist">
         {dummyData.map(item => {
           return <Todo task={item} />;
         })}
@@ -20,4 +41,20 @@ class TodoList extends React.Component {
   }
 }
 
-ReactDOM.render(<TodoList />, document.getElementById("root"));
+class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    };
+  }
+  componentDidMount() {
+    this.setState({
+      todos: dummyData
+    });
+  }
+  render() {
+    return <TodoList todos={this.state.todos} />;
+  }
+}
+ReactDOM.render(<TodoApp />, document.getElementById("root"));
